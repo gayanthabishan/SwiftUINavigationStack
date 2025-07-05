f85112f41378f26dc8a141110f8ad4b4367bf3a6
Bishan Meddegoda <bishan.meddegoda@pickme.lk>
2025-07-05 16:50:17 +0530

    Initial Commit

---
+++ SwiftUINavigationStack/SwiftUINavigationStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINavigationStackApp.swift
+//  SwiftUINavigationStack
+//
+//  Created by Bishanm on 2025-07-05.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINavigationStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
